31da716d34fe3c3e1b52cc4d4f3acd7fe4914a1d
Edwin Vautier <edwin.vautier@hetic.net>
2020-09-17 13:03:37 +0200

    Feat: Created the model for the customer.

---
+++ models/customers.go
@@ -0,0 +1,18 @@
+package models
+
+type Customer struct {
+	Number int
+	Name string
+	FirstName string
+	LastName string
+	Phone string
+	AddressLine1 string
+	AddressLine2 string
+	City string
+	State string
+	PostCode string
+	Country string
+	SalesRepEmployeeNumber int
+	CreditLimit float64
+}
+
